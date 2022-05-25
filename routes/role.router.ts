69c2c31370c61bd45549d10615499a22c4e6ba82
HaVietHung <haviethung0801@gmail.com>
2022-05-25 13:33:38 +0700

    Change3

--- routes/role.router.ts
+++ routes/role.router.ts
@@ -3,12 +3,20 @@ import RoleController from "../controlllers/role.controller";
 const { rightToAction } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
+router.delete(
+  "/role/:id",
+  rightToAction("delete-role"),
+  RoleController.deleteRole
+);
+
+router.put(
+  "/role/:id",
+  rightToAction("update-role"),
+  RoleController.updateRole
+);
+
 router.post("/role", rightToAction("create-role"), RoleController.createRole);
 
 router.get("/role", rightToAction("get-roles"), RoleController.getAllRoles);
-
-router.delete("/role", rightToAction("delete-role"), RoleController.deleteRole);
-
-router.put("/role", rightToAction("update-role"), RoleController.updateRole);
 
 export default router;
